50c46e5e6f0d9892f71b0c8d0754ac47fbe634de
Anisa <anisalopezruiz@gmail.com>
2021-03-03 12:13:28 -0600

    First commit

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    // needs to be a public void method with sole parameter View
+    public void sendMessage(View view) {
+        /** Intent provides runtime binding between separate components.
+         Represents app's intent to do something */
+        // Intent parameter takes in Context parameter and the Class parameter, in this case
+        // the activity to start 
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
