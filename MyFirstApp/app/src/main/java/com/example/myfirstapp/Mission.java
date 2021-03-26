589672508bbf2c9c949e3368fdb7484f36117e1c
Vivian-Xiao1025 <67659378+Vivian-Xiao1025@users.noreply.github.com>
2021-03-26 12:55:39 -0500

    mission page

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/Mission.java
@@ -0,0 +1,29 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+public class Mission extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    public void disable(View v){
+        v.setEnabled(false);
+
+    }
+}
